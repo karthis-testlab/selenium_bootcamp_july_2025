dff024bc4f4b1d873305baadcd3b03d8bed412cf
Karthikeyan Rajendran <karthis.testlab@gmail.com>
2025-08-24 13:09:16 +0530

    selenium bootcamp code

--- src/main/java/com/selenium/page/factory/script/demo/LoginPage.java
+++ src/main/java/com/selenium/page/factory/script/demo/LoginPage.java
@@ -1,11 +1,7 @@
 package com.selenium.page.factory.script.demo;
 
-import java.util.List;
-
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindAll;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.FindBys;
 import org.openqa.selenium.support.PageFactory;
 
 import com.selenium.testng.script.demo.TestNGHooks;
